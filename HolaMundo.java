d936d3128ba5a037c17bd40b1eb42902568b7d65
alvaro865 <asuasaca@unsa.edu.pe>
2022-04-29 17:18:33 -0500

    nuevo mensaje de bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
     public static void main(String[]args){
         //Hola mundo
         System.out.println("\nHola Mundo");
+        System.out.println("Empezando curso de pweb2");
     }
 }
